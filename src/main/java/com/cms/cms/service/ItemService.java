100b4fe5f59811146c41e6fc73c46f2090da62fe
bugFreeVaibhav <entertainmentforvaibhav@gmail.com>
2025-07-24 21:10:34 +0530

    updated Item Service

--- src/main/java/com/cms/cms/service/ItemService.java
+++ src/main/java/com/cms/cms/service/ItemService.java
@@ -5,12 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cms.cms.exception.CustomEntityNotFoundException;
 import com.cms.cms.models.common.OperationResponse;
@@ -19,7 +14,11 @@ import com.cms.cms.models.entity.Item;
 import com.cms.cms.repository.ItemRepository;
 import com.cms.cms.utils.CurrentUser;
 
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
+@Transactional
 public class ItemService {
 	private ItemRepository repo;
 	
