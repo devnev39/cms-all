a67775194e3240cb3a82440c70a3ae249502a75b
bugFreeVaibhav <entertainmentforvaibhav@gmail.com>
2025-07-24 21:11:56 +0530

    updated Order Service

--- src/main/java/com/cms/cms/service/OrderService.java
+++ src/main/java/com/cms/cms/service/OrderService.java
@@ -5,12 +5,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.cms.cms.exception.CustomEntityNotFoundException;
 import com.cms.cms.models.common.OperationResponse;
