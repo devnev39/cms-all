5342c7dd77fd4800bbf074e5547dee7140a69f33
bugFreeVaibhav <entertainmentforvaibhav@gmail.com>
2025-08-02 12:19:45 +0530

    vaibhav's commit

--- backend/src/main/java/com/cms/cms/security/SecurityConfig.java
+++ backend/src/main/java/com/cms/cms/security/SecurityConfig.java
@@ -51,7 +51,6 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.POST,"/user").permitAll()
                 .anyRequest()
                 .authenticated()
-            )
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
             .addFilterBefore(exceptionFilter, JwtFilter.class)
