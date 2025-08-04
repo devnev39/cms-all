e161a0e34ae5249efffe5ec636f01866a25a672b
bugFreeVaibhav <entertainmentforvaibhav@gmail.com>
2025-08-04 14:35:41 +0530

    fix: add itemReducer

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
 import catererReducer from "../features/user/catererSlice";
+import itemReducer from "../features/user/itemSlice";
 
 export default configureStore({
   reducer: {
     user: userReducer,
     caterer: catererReducer,
+    item: itemReducer,
   },
 });
