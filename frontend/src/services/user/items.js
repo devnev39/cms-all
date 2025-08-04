0a7751faeb5568b1f35af3c9585af583ad4ca166
bugFreeVaibhav <entertainmentforvaibhav@gmail.com>
2025-08-04 14:38:02 +0530

    fix: update some api

--- frontend/src/services/user/items.js
+++ frontend/src/services/user/items.js
@@ -2,6 +2,9 @@ import axios from "axios";
 import config from "../../config/config";
 
 export function getAllItems(catererId, token) {
+  console.log("Fetching items for caterer:", catererId);
+  console.log("Using token:", token);
+  console.log("****************************************");
   return axios.get(`${config.base_url}/item/caterer/${catererId}`, {
     headers: {
       Authorization: `Bearer ${token}`, 
@@ -10,7 +13,17 @@ export function getAllItems(catererId, token) {
 }
 
 export function createItem(item, token) {
+  console.log("Creating item:", item);
+  console.log("Using token:", token); 
+  console.log("****************************************");
   return axios.post(`${config.base_url}/item`, item, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+}
+export function getCatererId(userId, token) {
+  return axios.get(`${config.base_url}/caterer/user/${userId}`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
