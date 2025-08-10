81a05be3faad4e71f726c978ede3d8cd7d37721c
Bhuvanesh Bonde <devnev39@gmail.com>
2025-08-11 01:03:34 +0530

    feat: changed to deployed server

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
-  base_url: "http://localhost:8080",
-  // base_url: "http://13.203.79.101:80",
+  // base_url: "http://localhost:8080",
+  base_url: "http://13.203.79.101:80",
 };
 
 export default config;
