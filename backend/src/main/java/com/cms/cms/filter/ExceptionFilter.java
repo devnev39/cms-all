823adfe2bfd168ab5c30e84807d03ea92c78a039
Bhuvanesh Bonde <devnev39@gmail.com>
2025-08-11 01:19:57 +0530

    fix: updated static asset path

--- backend/src/main/java/com/cms/cms/filter/ExceptionFilter.java
+++ backend/src/main/java/com/cms/cms/filter/ExceptionFilter.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.cms.cms.exception.CustomEntityNotFoundException;
@@ -22,11 +21,11 @@ import jakarta.servlet.http.HttpServletResponse;
 public class ExceptionFilter extends OncePerRequestFilter {
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
         Map<String, String> mp = new HashMap<>();
         try {
-            filterChain.doFilter(request, response);   
+            filterChain.doFilter(request, response);
         } catch (JwtException jw_ex) {
             jw_ex.printStackTrace();
             mp.put("error", "JWT Error !");
@@ -39,28 +38,29 @@ public class ExceptionFilter extends OncePerRequestFilter {
             mp.put("error", "Internal server error");
             mp.put("detail", ex.getMessage());
             response.setStatus(401);
-            response.getWriter().write(convertObjectToJson(mp)); 
+            response.getWriter().write(convertObjectToJson(mp));
         }
         // catch (HttpClientErrorException ex) {
-        //     System.out.println("Exception occured !");
-        //     ex.printStackTrace();
-        //     mp.put("error", "Client Error");
-        //     mp.put("detail", ex.getLocalizedMessage());
-        //     response.setStatus(ex.getStatusCode().value());
-        //     response.getWriter().write(convertObjectToJson(mp));
+        // System.out.println("Exception occured !");
+        // ex.printStackTrace();
+        // mp.put("error", "Client Error");
+        // mp.put("detail", ex.getLocalizedMessage());
+        // response.setStatus(ex.getStatusCode().value());
+        // response.getWriter().write(convertObjectToJson(mp));
         // }
         // catch (Exception ex) {
-        //     System.out.println("Exception occured !");
-        //     ex.printStackTrace();
-        //     mp.put("error", "Internal server error");
-        //     mp.put("detail", ex.getMessage());
-        //     response.setStatus(500);
-        //     response.getWriter().write(convertObjectToJson(mp));
+        // System.out.println("Exception occured !");
+        // ex.printStackTrace();
+        // mp.put("error", "Internal server error");
+        // mp.put("detail", ex.getMessage());
+        // response.setStatus(500);
+        // response.getWriter().write(convertObjectToJson(mp));
         // }
     }
 
     public String convertObjectToJson(Object obj) throws JsonProcessingException {
-        if (obj == null) return null;
+        if (obj == null)
+            return null;
         ObjectMapper mp = new ObjectMapper();
         return mp.writeValueAsString(obj);
     }
